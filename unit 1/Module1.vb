702dc8e0b0866c61d1e6766483d47c5d700307ec
taufan-mft <taufanderazor@gmail.com>
2019-04-07 09:43:46 +0700

    Create Module1.vb

---
+++ unit 1/Module1.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        For i = 1 To 100
+            If i Mod 2 = 0 Then
+                Console.WriteLine(i)
+            End If
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
